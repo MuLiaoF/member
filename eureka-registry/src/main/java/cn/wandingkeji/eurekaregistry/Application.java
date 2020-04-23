aa242ffed1bef1c7aab09df6bfd5ebcc9e154db7
myliao <fengx8044@gmail.com>
2020-04-23 18:14:09 +0800

    权限管理

--- eureka-registry/src/main/java/cn/wandingkeji/eurekaregistry/Application.java
+++ eureka-registry/src/main/java/cn/wandingkeji/eurekaregistry/Application.java
@@ -3,10 +3,12 @@ package cn.wandingkeji.eurekaregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
--- link/src/main/java/cn/wandingkeji/link/LinkApplication.java
+++ link/src/main/java/cn/wandingkeji/link/LinkApplication.java
@@ -2,12 +2,14 @@ package cn.wandingkeji.link;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZipkinServer
+@EnableDiscoveryClient
 public class LinkApplication {
 
     public static void main(String[] args) {
